8fcd47e587c555310a4018bbaa6afa4fae2e193f
Tom Holmes <tholmes59@gmail.com>
2019-01-23 01:27:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,44 @@
 # Make your shoe class here!
 class Shoe 
   
-  def initialize 
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(color)
+    @color = color
+  end
+  
+  def color
+    @color
+  end 
+  
+  def size=(size)
+    @size = size
+  end
+  
+  def size
+    @size
+  end
+  
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
   
 end
